de2cc4830a75bbd09f7cdaee56906f162c84938a
xiaoyu <tanyusheng@outlook.com>
2021-08-26 23:19:07 +0800

    回文链表

--- 递归/_203_移除链表元素.java
+++ 递归/_203_移除链表元素.java
@@ -1,5 +1,7 @@
 package 递归;
 
+import java.util.Scanner;
+
 public class _203_移除链表元素 {
 	public class ListNode {
 	    int val;
@@ -8,12 +10,21 @@ public class _203_移除链表元素 {
 	    ListNode(int val) { this.val = val; }
 	    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 	}
-	public ListNode removeElements(ListNode head, int val) {
-        ListNode dummyNode = new ListNode(0);
-        dummyNode.next = head;
-        ListNode prev = dummyNode;
-        while(head.next!=null) {
-        	
-        }
-    }
+//	public ListNode removeElements(ListNode head, int val) {
+//        ListNode dummyNode = new ListNode(0);
+//        dummyNode.next = head;
+//        ListNode prev = dummyNode;
+//        while(head.next!=null) {
+//        	
+//        }
+//    }
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int a,b;
+		while(cin.hasNextInt()) {
+			a = cin.nextInt();
+			b = cin.nextInt();
+			System.out.println(a+b);
+		}
+	}
 }
---
+++ 链表/_234_回文链表.java
@@ -0,0 +1,5 @@
+package 链表;
+
+public class _234_回文链表 {
+
+}

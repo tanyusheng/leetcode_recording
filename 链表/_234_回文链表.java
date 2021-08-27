3db7e6aaa88838ce2a1d1a42292aa9555e2fe088
xiaoyu <tanyusheng@outlook.com>
2021-08-27 23:12:50 +0800

    递归

--- 链表/_234_回文链表.java
+++ 链表/_234_回文链表.java
@@ -1,5 +1,11 @@
 package 链表;
-
+/**
+ * https://leetcode-cn.com/problems/palindrome-linked-list/
+ * @author swift
+ *
+ */
 public class _234_回文链表 {
-
+	public boolean isPalindrome(ListNode head) {
+        return false;
+    }
 }

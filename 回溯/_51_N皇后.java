c4de1f6331a62c5b90fcd21191353d93dec2b3f7
xiaoyu <tanyusheng@outlook.com>
2021-09-01 22:02:55 +0800

    回溯

---
+++ 回溯/_51_N皇后.java
@@ -0,0 +1,5 @@
+package 回溯;
+
+public class _51_N皇后 {
+
+}
--- 递归/汉诺塔问题.java
+++ 递归/汉诺塔问题.java
@@ -1,5 +1,24 @@
 package 递归;
 
-public class 汉诺塔问题 {
+import java.util.List;
 
+public class 汉诺塔问题 {
+	
+	public void hanota(List<Integer> A, List<Integer> B, List<Integer> C) {
+        move(A.size(), A, B, C);
+    }
+	
+	private void move(int n,List<Integer> A, List<Integer> B, List<Integer> C) {
+		if(n==1) {
+			do_move(A, C);
+		}else {
+			move(n-1, A, C, B);
+			do_move(A, C);
+			move(n-1, B, A, C);
+		}
+		
+	}
+	private void do_move(List<Integer> start, List<Integer> over) {
+		over.add(start.remove(start.size()-1));
+	}
 }

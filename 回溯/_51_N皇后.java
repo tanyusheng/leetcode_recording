452c0805e8a3b07067fea9720046d847c46453d4
xiaoyu <tanyusheng@outlook.com>
2021-09-02 22:37:12 +0800

    N皇后

--- 回溯/_51_N皇后.java
+++ 回溯/_51_N皇后.java
@@ -1,7 +1,9 @@
 package 回溯;
 
+import java.util.List;
+
 public class _51_N皇后 {
 	public List<List<String>> solveNQueens(int n) {
-		
+		return null;
     }
 }
---
+++ 回溯/_52_N皇后II.java
@@ -0,0 +1,7 @@
+package 回溯;
+
+public class _52_N皇后II {
+	public int totalNQueens(int n) {
+		return 0;
+    }
+}

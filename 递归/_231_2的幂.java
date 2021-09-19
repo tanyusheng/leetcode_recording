173d8e6edae7806a17de7b0e626a4c2d90feefb7
xiaoyu <tanyusheng@outlook.com>
2021-09-19 22:37:21 +0800

    2的幂

--- 递归/_231_2的幂.java
+++ 递归/_231_2的幂.java
@@ -1,5 +1,37 @@
 package 递归;
 
+import java.util.Scanner;
+/**
+ * https://leetcode-cn.com/problems/power-of-two/
+ * @author swift
+ *
+ */
 public class _231_2的幂 {
-
+	/**
+	 * 递归法
+	 * @param n
+	 * @return
+	 */
+	public static boolean isPowerOfTwo(int n) {
+		if(n<=0) return false;
+		else if(n==1) return true;
+		else return (n%2 == 0) && isPowerOfTwo(n/2);
+	}
+	
+	/**
+	 * 如果一个数是2的幂，则n & n-1 == 0
+	 * @param n
+	 * @return
+	 */
+	public static boolean isPowerOfTwo1(int n) {
+		return n>0 && (n & (n-1)) == 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner cinScanner = new Scanner(System.in);
+		while(cinScanner.hasNext()) {
+			int n = cinScanner.nextInt();
+			System.out.println(isPowerOfTwo(n));
+		}
+	}
 }

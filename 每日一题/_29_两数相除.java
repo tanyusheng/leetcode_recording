f0d316c40bf3a56598fedac2f1b88e5ae6b6fb40
xiaoyu <tanyusheng@outlook.com>
2021-10-12 23:44:31 +0800

    每日一题

---
+++ 每日一题/_29_两数相除.java
@@ -0,0 +1,32 @@
+package 每日一题;
+
+public class _29_两数相除 {
+    int INF = Integer.MAX_VALUE;
+    public int divide(int _a, int _b) {
+        long a = _a, b = _b;
+        boolean flag = false;
+        if ((a < 0 && b > 0) || (a > 0 && b < 0)) flag = true;
+        if (a < 0) a = -a;
+        if (b < 0) b = -b;
+        long l = 0, r = a;
+        while (l < r) {
+            long mid = l + r + 1 >> 1;
+            if (mul(mid, b) <= a) l = mid;
+            else r = mid - 1;
+        }
+        r = flag ? -r : r;
+        if (r > INF || r < -INF - 1) return INF;
+        return (int)r;
+    }
+    long mul(long a, long k) {
+        long ans = 0;
+        while (k > 0) {
+            if ((k & 1) == 1) ans += a;
+            k >>= 1;
+            a += a;
+        }
+        return ans;
+    }
+}
+
+

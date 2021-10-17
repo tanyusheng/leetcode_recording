f629c22eb885e57860096f73e5e1f228f2ebf716
xiaoyu <tanyusheng@outlook.com>
2021-10-17 22:48:46 +0800

    每日一题

---
+++ 每日一题/_230_二叉搜索树中的第K小的元素.java
@@ -0,0 +1,25 @@
+package 每日一题;
+
+
+/**
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+ * @author swift
+ *
+ */
+public class _230_二叉搜索树中的第K小的元素 {
+	public int kthSmallest(TreeNode root, int k) {
+		
+		return 0;
+    }
+	
+	private TreeNode createBt(int[] arr,int i) {
+		TreeNode root = null;
+		if(i > arr.length) {
+			return null;
+		}
+		root = new TreeNode(arr[i]);
+		root.left = createBt(arr, 2*i+1);
+		root.right = createBt(arr, 2*i+2);
+		return root;
+	}
+}

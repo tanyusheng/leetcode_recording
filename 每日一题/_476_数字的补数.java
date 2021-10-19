8ad9ee92701f8f0c8a1d76aa8f3764c8bfa94dd0
xiaoyu <tanyusheng@outlook.com>
2021-10-19 10:24:27 +0800

    位运算

--- 每日一题/_476_数字的补数.java
+++ 每日一题/_476_数字的补数.java
@@ -5,38 +5,23 @@ import java.util.Scanner;
 /**
  * https://leetcode-cn.com/problems/number-complement/
  * @author swift
- * 	思路：把10进制转为2进制，取反后再转10进制
+ * 	思路：常规法：把10进制转为2进制，取反后再转10进制
+ * 
  */
 public class _476_数字的补数 {
+	
 	public static int findComplement(int num) {
 		
-		return deToBi(num);
+		int mask = 1 << 30;
+		while((mask &  num) == 0) {
+			mask >>= 1;
+		}
+		mask = (mask << 1) - 1;
+		return num ^ mask;
+		
     }
 	
-	private static int deToBi(int n) {
-		String biString = Integer.toBinaryString(n);
-		char[] biStringArray = biString.toCharArray();
-		StringBuilder sb = new StringBuilder();
-		for(char c : biStringArray) {
-			if(c == 0) {
-				sb.append(1);
-			}else {
-				sb.append(0);
-			}
-		}
-		// 反转后的01字符串
-		String rbiString = sb.toString();
-		char[] rbiArray = rbiString.toCharArray();
-		int sum = 0;
-		int len = rbiArray.length;
-		for (int i =0;i<len;i++) {
-			if(rbiArray[i]=='1') {
-				sum += Math.pow(2, i);
-			}
-			
-		}
-		return sum;
-	}
+	
 	
 	public static void main(String[] args) {
 		Scanner cinScanner = new Scanner(System.in);

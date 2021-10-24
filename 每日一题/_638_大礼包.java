94a9c03b1340b36289f534c59e4cf7ff39ecf35b
swift <tanyusheng@outlook.com>
2021-10-24 22:16:51 +0800

    每日一题

---
+++ 每日一题/_638_大礼包.java
@@ -0,0 +1,15 @@
+package 每日一题;
+
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/shopping-offers/
+ * @author swift
+ * 思路：
+ */
+public class _638_大礼包 {
+	public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+		
+		return 0;
+    }
+}

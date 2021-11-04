8826cf6ddad6703b1b661282e00417a553ac0b9c
xiaoyu <tanyusheng@outlook.com>
2021-11-04 20:38:57 +0800

    每日一题

---
+++ 每日一题/_367_有效的完全平方数.java
@@ -0,0 +1,25 @@
+package 每日一题;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author swift
+ *https://leetcode-cn.com/problems/valid-perfect-square/
+ */
+public class _367_有效的完全平方数 {
+	public static boolean isPerfectSquare(int num) {
+		int res =(int) Math.sqrt(num);
+        return res*res == num;
+    }
+	
+	public static void main(String[] args) {
+		Scanner cinScanner = new Scanner(System.in);
+		while(cinScanner.hasNext()) {
+			int num = cinScanner.nextInt();
+			boolean flag = isPerfectSquare(num);
+			System.out.println(flag);
+		}
+	}
+
+}

4f6a27cd4b146e7f2ece985f9f6458ba6e1ceede
xiaoyu <tanyusheng@outlook.com>
2021-11-05 22:24:56 +0800

    每日一题

---
+++ 每日一题/_1218_最长定差子序列.java
@@ -0,0 +1,13 @@
+package 每日一题;
+
+/**
+ * https://leetcode-cn.com/problems/longest-arithmetic-subsequence-of-given-difference/solution/
+ * @author swift
+ *
+ */
+public class _1218_最长定差子序列 {
+	public int longestSubsequence(int[] arr, int difference) {
+		
+		return 0;
+    }
+}

46e8beec257c00fbf3b342e12f0469e835a388de
xiaoyu <tanyusheng@outlook.com>
2021-11-11 20:05:39 +0800

    每日一题

---
+++ 每日一题/_629_K个逆序对数组.java
@@ -0,0 +1,24 @@
+package 每日一题;
+
+import java.util.Scanner;
+
+/**
+ * https://leetcode-cn.com/problems/k-inverse-pairs-array/solution/
+ * @author swift
+ *	思路：我猜用的是动态规划，但是这题真不会写！
+ */
+public class _629_K个逆序对数组 {
+	public int kInversePairs(int n, int k) {
+		
+		return 0;
+    }
+	
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while(scanner.hasNext()) {
+			int n = scanner.nextInt();
+			int k = scanner.nextInt();
+		}
+	}
+}

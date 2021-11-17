c91f22b4ccc992cac0f6d71e124380d62d6f85ad
xiaoyu <tanyusheng@outlook.com>
2021-11-17 15:18:14 +0800

    每日一题

---
+++ 每日一题/_318_最大单词长度乘积.java
@@ -0,0 +1,40 @@
+package 每日一题;
+
+import java.util.Scanner;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-product-of-word-lengths/
+ * @author swift
+ *
+ */
+public class _318_最大单词长度乘积 {
+	public static int maxProduct(String[] words) {
+		int n = words.length;
+		// 先转化为bit数组
+		int[] bits = new int[n];
+		for (int i = 0; i < n; i++) {
+			char[] chars = words[i].toCharArray();
+			for (int j = 0; j < chars.length; j++) {
+				bits[i] |= 1 << (chars[j] - 'a');
+			}
+		}
+		int len = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < n; j++) {
+				if((bits[i] & bits[j]) == 0) {
+					len = Math.max(len, words[i].length()*words[j].length());
+				}
+			}
+		}
+		return len;
+    }
+	
+	public static void main(String[] args) {
+		Scanner cinScanner = new Scanner(System.in);
+		while(cinScanner.hasNext()) {
+			String str = cinScanner.next();
+			String[] strs = str.split(",");
+			System.out.println(maxProduct(strs));
+		}
+	}
+}

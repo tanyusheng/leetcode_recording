f8f8b0d6697dea6c655588492a374b5604633fdd
swift <tanyusheng@outlook.com>
2021-12-15 23:24:04 +0800

    每日一题

---
+++ 每日一题12月/_851_喧闹与富有.java
@@ -0,0 +1,28 @@
+package 每日一题12月;
+
+import java.util.Scanner;
+/**
+ * https://leetcode-cn.com/problems/loud-and-rich/
+ * @author swift
+ *
+ */
+public class _851_喧闹与富有 {
+	public int[] loudAndRich(int[][] richer, int[] quiet) {
+		
+		return null;
+    }
+	public static void main(String[] args) {
+		Scanner cinScanner = new Scanner(System.in);
+		while(cinScanner.hasNext()) {
+			int m = cinScanner.nextInt();
+			int n = cinScanner.nextInt();
+			cinScanner.nextLine();
+			int[][] richer = new int[m][n];
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					richer[i][j] = cinScanner.nextInt();
+				}
+			}
+		}
+	}
+}

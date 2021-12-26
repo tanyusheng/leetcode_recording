90daf04b6361cde73d4b70ac1756aceb6078cf61
xiaoyu <tanyusheng@outlook.com>
2021-12-26 17:52:43 +0800

    每日一题

---
+++ 每日一题12月/_1078_Bigram分词.java
@@ -0,0 +1,24 @@
+package 每日一题12月;
+
+import java.util.Scanner;
+
+/**
+ * https://leetcode-cn.com/problems/occurrences-after-bigram/
+ * @author swift
+ *
+ */
+public class _1078_Bigram分词 {
+	public String[] findOcurrences(String text, String first, String second) {
+		
+		return null;
+    }
+	public static void main(String[] args) {
+		Scanner cinScanner = new Scanner(System.in);
+		while(cinScanner.hasNext()) {
+			String text = cinScanner.next();
+			String first = cinScanner.next();
+			String second = cinScanner.next();
+		}
+	}
+}
+

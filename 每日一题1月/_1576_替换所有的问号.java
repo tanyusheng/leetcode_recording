0247453e7e9ad114fd68021ba2a01ed7c3978ef9
swift <tanyusheng@outlook.com>
2022-01-05 23:31:24 +0800

    每日一题

---
+++ 每日一题1月/_1576_替换所有的问号.java
@@ -0,0 +1,24 @@
+package 每日一题1月;
+
+import java.util.Scanner;
+
+
+/**
+ * https://leetcode-cn.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters/
+ * @author swift
+ *
+ */
+public class _1576_替换所有的问号 {
+	public static String modifyString(String s) {
+		
+		return null;
+    }
+	public static void main(String[] args) {
+		Scanner cinScanner = new Scanner(System.in);
+		while(cinScanner.hasNext()) {
+			String string = cinScanner.next();
+			String res = modifyString(string);
+			System.out.println(res);
+		}
+	}
+}

1e507d279e9089673c4d09bbfbc0a0449a594e79
swift <tanyusheng@outlook.com>
2022-01-06 23:49:10 +0800

    每日一题

--- 每日一题1月/_1576_替换所有的问号.java
+++ 每日一题1月/_1576_替换所有的问号.java
@@ -10,8 +10,25 @@ import java.util.Scanner;
  */
 public class _1576_替换所有的问号 {
 	public static String modifyString(String s) {
+		int last = 0;
+        int n = s.length();
+        char[] ch = s.toCharArray();
+        
+        for(int i = 0; i < n; i++) {
+            if (ch[i] == '?') {
+                ch[i] = (char)('a'+last);
+                last = (last+1)%26;
+            } else {
+                last = (ch[i]-'a'+1)%26;
+            }
+
+            if (i < n-1 && ch[i] == ch[i+1]) {
+                ch[i] = (char) ('a'+(ch[i]-'a'+1)%26);
+            }
+        }
+
+        return new String(ch);
 		
-		return null;
     }
 	public static void main(String[] args) {
 		Scanner cinScanner = new Scanner(System.in);
